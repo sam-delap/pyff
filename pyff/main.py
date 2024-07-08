15d4ce7f8f4b8318b69d5697bba69192f1e9f36a
Sam DeLap <sjcdelap@gmail.com>
2024-07-08 18:05:30 -0400

    Fill func, fixing games started

--- pyff/main.py
+++ pyff/main.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from .teams import Team
 from .positions import QB, SkillPlayer
 
@@ -44,7 +46,7 @@ def main(filename: str, teams: list[str]=["all"]) -> None:
             team.project()
             team.save_projections(filename)
         project_teams_players(team, filename)
-        fill_team_stats(team, filename)
+        fill_team_stats(team_name, filename)
 
 def project_teams_players(team: Team, filename: str) -> None:
     if input(f'Do you need to do QB projections for {team.team_name}? ') == 'y':
@@ -92,5 +94,55 @@ def te_prompt_loop(team: Team, filename: str):
             te.save_projections(filename)
         should_continue = input(f'Would you like to project another TE for {team.team_name}? ') == 'y'
 
-def fill_team_stats(team: Team, filename: str):
-    pass
+def fill_team_stats(team_name: str, filename: str):
+    team_df = pd.read_excel(filename, sheet_name=team_name.capitalize())
+
+    run_plays = team_df['Run Plays'][0]
+    pass_plays = team_df['Pass Plays'][0]
+
+    # Remainder calculation
+    other_players_index = team_df['Player Name'].size + 2
+    team_df.loc[other_players_index, 'Pos'] = 'WR/RB/TE'
+    team_df.loc[other_players_index, 'Player Name'] = 'Other Players'
+    team_df.loc[other_players_index, 'Games Started'] = 17
+    team_df.loc[other_players_index, 'Rush Share'] = 100 - team_df['Rush Share'].sum()
+    if team_df.loc[other_players_index, 'Rush Share'] < 0:
+        print(f'Too many carries allocated for team {team_name}.')
+    team_df.loc[other_players_index, 'Yards/Carry'] = 4.2
+    team_df.loc[other_players_index, 'TDs/Yard'] = 0.006
+    team_df.loc[other_players_index, 'Target Share'] = 100 - team_df['Target Share'].sum()
+    if team_df.loc[other_players_index, 'Target Share'] < 0:
+        print(f'Too many targets allocated for team {team_name}.')
+    team_df.loc[other_players_index, 'Catch Percentage'] = 62
+    team_df.loc[other_players_index, 'Yards/Catch'] = 11
+    team_df.loc[other_players_index, 'TDs/receiving yard'] = 0.006
+
+    # Rushing stats
+    team_df['Carries'] = team_df['Rush Share'] / 100 * run_plays
+    team_df['Rushing Yards'] = team_df['Yards/Carry'] * team_df['Carries']
+    team_df['Rushing TDs'] = team_df['Rushing Yards'] * team_df['TDs/Yard']
+
+    # Receiving stats
+    team_df['Targets'] = team_df['Target Share'] / 100 * pass_plays
+    team_df['Receptions'] = team_df['Targets'] * team_df['Catch Percentage'] / 100
+    team_df['Receiving Yards'] = team_df['Receptions'] * team_df['Yards/Catch']
+    team_df['Receiving TDs'] = team_df['Receiving Yards'] * team_df['TDs/receiving yard']
+
+    # Passing stats (QB-dependent)
+    is_qb = team_df['Pos'] == 'QB'
+    team_df.loc[is_qb, 'Passing Attempts'] = team_df.loc[is_qb, 'Games Started'] / 17 * pass_plays
+    print(team_df['Passing Attempts'])
+    team_df.loc[is_qb, 'Interceptions'] = team_df.loc[is_qb, 'Passing Attempts'] * team_df[is_qb, 'Interception %'] / 100
+
+    # Passing stats (skill player-dependent)
+    team_df.loc[is_qb, 'Completions'] = team_df['Receptions'].sum() * team_df['Games Started'] / 17
+    team_df.loc[is_qb, 'Completion %'] = team_df['Completions'] / team_df['Passing Attempts'] * 100
+    if (team_df['Completion %'] > 70).any():
+        print(f'Completion percentage too high for team {team_name}')
+    team_df.loc[is_qb, 'Passing Yards'] = team_df['Receiving Yards'].sum() * team_df['Games Started'] / 17
+    team_df.loc[is_qb, 'Passing TDs'] = team_df['Receiving TDs'].sum() * team_df['Games Started'] / 17
+    team_df.loc[is_qb, 'Passing TD %'] = team_df['Passing TDs'] / team_df['Passing Attempts'] * 100
+    if (team_df['Passing TD %'] > 8).any():
+        print(f'Passing TD percentage too high for team {team_name}')
+
+    print(team_df.head(5))
